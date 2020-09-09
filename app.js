b3a76167d1b1f8ccb93abbbd1261c642af0d8879
mvillanuevas <37197206+mvillanuevas@users.noreply.github.com>
2020-09-09 00:02:41 -0500

    make it better

--- app.js
+++ app.js
@@ -36,9 +36,6 @@ if (!config.FB_APP_SECRET) {
 if (!config.SERVER_URL) { //used for ink to static files
     throw new Error('missing SERVER_URL');
 }
-if (!config.SENGRID_API_KEY) { //sending email
-    throw new Error('missing SENGRID_API_KEY');
-}
 if (!config.EMAIL_FROM) { //sending email
     throw new Error('missing EMAIL_FROM');
 }
